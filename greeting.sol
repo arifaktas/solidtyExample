74b23f54fe030900abeda3aee14f9bec00183023
arifaktas <122029041+arifaktas@users.noreply.github.com>
2023-01-05 16:31:24 +0300

    Update greeting.sol

--- greeting.sol
+++ greeting.sol
@@ -1,1 +1,7 @@
- 
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.4;
+
+contract HelloWorld {
+    string public greeting = "Hello World!";
+}
